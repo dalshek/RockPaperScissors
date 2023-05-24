63f3743c0d4c24ece335e1a65c35bf849120eed3
dalshek <cdiezpag@gmail.com>
2023-05-24 23:32:54 +0200

    JS finished, first part, program running ok in console

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
-let user; 
-let computer;
+let user, computer;
+let userScore = 0;
+let computerScore = 0;
+
 
 let getComputerChoice = () => {
     let random = Math.random();
@@ -15,24 +17,55 @@ let askUserChoice = () => console.log("Rock, Paper or Scissors?");
 let getUserchoice = () => {
     
     user=prompt("Rock, Paper or Scissors????");
-    return user;
+    return user.toLowerCase();
 }
 
 
-let game = (user, computer) => {
-
+let playRound = (user, computer) => {
     if (user == computer) return "Draw";
-    else if(user=="rock" && computer == "paper") return "You Lose! Rock beats Paper";
-    else if(user=="paper" && computer == "rock") return "You Win! Rock beats Paper";
-    else if(user=="rock" && computer == "scissors") return "You Win! Scissors beats Rock";
-    else if(user=="scissors" && computer == "rock") return "You Lose! Scissors beats Rock";
-    else if(user=="paper" && computer == "scissors") return "You Lose! Scissors beats Paper";
-    else if(user=="scissors" && computer == "paper") return "You Lose! Scissors beats Paper";
+    else if(user=="rock" && computer == "paper"){
+        computerScore++;
+        return "You Lose! Rock beats Paper";
+    }
+    else if(user=="paper" && computer == "rock"){ 
+        userScore++;
+        return "You Win! Rock beats Paper";
+    }
+    else if(user=="rock" && computer == "scissors"){
+        userScore++;
+        return "You Win! Scissors beats Rock";
+    }
+    else if(user=="scissors" && computer == "rock"){ 
+        computerScore++;
+        return "You Lose! Scissors beats Rock";
+    }
+    else if(user=="paper" && computer == "scissors"){ 
+        computerScore++;
+        return "You Lose! Scissors beats Paper";
+    }
+    else if(user=="scissors" && computer == "paper"){
+        userScore++;
+        return "You Win! Scissors beats Paper";
+    }
     else return "Not a correct option introduced";
 }
 
+let game = () => {
+    for(let i=0; i<5; i++) {
+        console.log(playRound(getUserchoice(), getComputerChoice()));
+    }
+}
+
+let whoWin = (user, computer) => {
+    if (user<computer) return console.log("LOOOOOOSER");
+    else if (user>computer) return console.log("YOU FUCKING WIN");
+    else return console.log("DRAAAAW :(")
+}
+
+game();
+whoWin(userScore, computerScore);
 
 
-console.log(game(getUserchoice(), getComputerChoice()));
+//console.log(playRound(getUserchoice(), getComputerChoice()));
 
-console.log("CONSOLE: " + computer + "\t USER: " + user)
+//console.log("CONSOLE: " + computer + "\t USER: " + user)
